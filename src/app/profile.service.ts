8e83ce95e87d06d1f14f8e38fce7af7a35597a90
evantoh <evanmwenda@gmail.com>
2018-01-12 13:02:47 +0300

    inject http module and create propertty to use

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -4,6 +4,17 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProfileService {
 
-  constructor() { }
+private username:string;
+private clientid:"Iv1.8438a733dbb29966";
+private clientsecret:" 21b7ed833c042c602356c13e7d573e26c060235b";
+  constructor(private http:Http) {
+console.log("service is ready");
+this.username='evantoh';
+  }
+  getprofileInfo(){
+    return this.http.get("https://api.github.com/users/"
+     + this.username + "?client_id="+ this.clientid +"&client_secret=" +this.clientsecret )
+     .map(res=>res.json());
+  }
 
 }
