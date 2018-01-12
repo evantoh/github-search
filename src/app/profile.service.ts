613c87e218dbeac2dd80a90a3c71308fac89b70a
evantoh <evanmwenda@gmail.com>
2018-01-12 12:00:42 +0300

    add profile service to the project

---
+++ src/app/profile.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import{Http,Headers} from'@angular/http';
+import 'rxjs/add/operator/map';
+@Injectable()
+export class ProfileService {
+
+  constructor() { }
+
+}
