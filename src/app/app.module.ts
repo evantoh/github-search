1410a2155ff0ab94ebf9fcb4c87b9432aea2a4d3
evantoh <evanmwenda@gmail.com>
2018-01-12 11:52:07 +0300

    add a profile component which loads the personal data from github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
