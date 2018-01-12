75dde0ccbe47bfc6e9f8c3b8d746311084be5587
evantoh <evanmwenda@gmail.com>
2018-01-12 14:06:21 +0300

    add necessary imports to display github details in the console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{ProfileService} from './profile.service';
+import{HttpModule} from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -9,12 +11,14 @@ import { ProfileComponent } from './profile/profile.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      HttpModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ProfileService} from '../profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private profileserve :ProfileService ) {
+  this.profileserve.getprofileInfo().subscribe(profile=>{
+    console.log(profile);
+  }
+  ) }
 
   ngOnInit() {
   }
