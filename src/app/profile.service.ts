00ed500bf781b0c8ea4c5ba28113d85e0d3f6bad
evantoh <evanmwenda@gmail.com>
2018-01-17 00:03:52 +0300

    space and ident the code for readability purpose

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -7,7 +7,6 @@ export class ProfileService {
 
 
 private username:string;
-// private clientid:"Iv1.8438a733dbb29966";
 private apikey:string ="1b7369cc13d46c9e10c744f9b8556ca9ad44df69";
   constructor(private http:Http) {
 console.log("service is ready");
@@ -21,11 +20,7 @@ this.username='evantoh';
       return this.http.get("https://api.github.com/users/"+ this.username + "/repos" +"?access_token=" +this.apikey)
        .map(res=>res.json());
     }
-  // getprofileRepos(){
-  //   return this.http.get("https://api.github.com/users/"
-  //    + this.username + "/repos?client_id="+ this.clientid +"&client_secret=" +this.clientsecret )
-  //    .map(res=>res.json());
-  // }
+
   updateprofile(username:string){
     this.username=username;
   }
